f9a195be987588e20feceed426a8061c25d3f050
Hasan Ashab <hasanashab.18205@gmail.com>
2026-07-26 16:23:44 +0600

    fix(autoload): resolve PSR-4 namespace mismatches

--- app/Listeners/SendAccountVerificationNotification.php
+++ app/Listeners/SendAccountVerificationNotification.php
@@ -5,7 +5,7 @@ namespace App\Listeners;
 use App\Actions\Auth\SendVerificationOtp;
 use Illuminate\Auth\Events\Registered;
 
-class SendOneTimePassword
+class SendAccountVerificationNotification
 {
     public function __construct(
         protected SendVerificationOtp $sendVerificationOtp,
